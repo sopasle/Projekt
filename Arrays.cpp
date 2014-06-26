6883454a98085d479a65b9a066579a115075f1eb
oliver.pasler@uni-ulm.de <op2@wsl38.informatik.uni-ulm.de>
2014-06-26 14:33:04 +0200

    Test

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,20 +1,45 @@
-#include <iostream>
 #include <vector>
-#include <cstring>
 #include <cmath>
+#include <iostream>
+#include <utility>
+#include <cstring>
+/*
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/int_vector.hpp>
+#include <sdsl/suffix_arrays.hpp>
+#include <sdsl/wavelet_trees.hpp>
+#include <sdsl/rmq_support.hpp>
+*/
+
+
 using namespace std;
+//using namespace sdsl;
 
 
+/*
+ * OLI
+*/
 int d_Array(int d[], int size_d,int is[], int ie[]);
 int d_Strich(int d_1[],int size_d,int SAr[], int d[]);
 
+/* 
+ * SANDRA
+ *
+void searchPattern(int st,int ed, int_vector<> dStrich, int patternLength, csa_wt<> sa, int g[], int is[], int size_is, int ie[]);
+void getFactors(int startIndex, int patternLength, int g[], int is[], int size_is, int ie[]);
+*/
 int main(){
-
+/*
+ * OLI
+ */
 	int d[9];
 	int d_1[9];
+	int SAr[9];
+	
+	
 	int is[8];	
 	int ie[8];	
-	int SAr[9];
+
 	is[0] = 0;	ie[0] = 1;
 	is[1] = 0;	ie[1] = 2;
 	is[2] = 0;	ie[2] = 3;
@@ -40,9 +65,37 @@ int main(){
 	for(int i = 0; i<sizeof(d) / sizeof(int); i++){
 	cout << "d: " << d[i]<< " SAr: " << SAr[i]+1 << " d_1: " << d_1[i] << endl;
 }
+}
+
+/*
+ * SANDRA 
+ *
+  int_vector<> dStrich = {4,2,4,3,1,5,2,3,6};
+  int g[] = {1,2,3,6,8,5,7,4};
+  int is[] = {0,0,0,2,3,4,6,7};
+  int size_is = sizeof(is)/sizeof(is[0]);
+  int ie[] = {1,2,3,3,8,5,7,8};
+  
+  csa_wt<> sa;
+  construct_im(sa, "acgtgatag", 1);	//Eingabe: Referenzstring
+
+  string pattern = "a";	//Eingabe: Pattern
+  
+  uint64_t i=0, j=sa.size()-1, startIndex=0, endIndex=0;
+  backward_search(sa, i, j, pattern.begin(), pattern.end(), startIndex, endIndex); // Rueckwaertssuche => startIndex, endIndex
+  searchPattern(startIndex-1, endIndex-1, dStrich,pattern.size(), sa, g, is, size_is, ie);
+
+
+
 	return 0;
 	
 }
+
+
+/*
+ * OLI
+ */
+
 
 int d_Array(int d[],int size_d,int is[], int ie[]){
 	int p = 0; 							// Aktuelle Stelle in d
@@ -76,5 +129,39 @@ int d_Strich(int d_1[] ,int size_d,int SAr[], int d[]){
 		d_1[i] = d[SAr[i]];				// Berechnung von d', Länge des längsten Intervalls an der Position SAr[i]
 	}
 	
-	
+	return 0;
 }
+
+
+/*
+ * SANDRA
+ *
+
+void searchPattern(int st,int ed, int_vector<> dStrich, int patternLength, csa_wt<> sa, int g[], int is[], int size_is, int ie[]){
+  rmq_succinct_sct<false> rmaxq(&dStrich);
+  int maxPosition = rmaxq(st,ed);
+  if(dStrich[maxPosition] >= patternLength){ //Abbruch, wenn Faktorlänge < Patternlänge
+	getFactors(sa[maxPosition+1], patternLength, g, is, size_is, ie);
+    if(st < maxPosition) 
+      searchPattern(st, maxPosition-1, dStrich, patternLength, sa, g, is, size_is, ie);
+    if(ed > maxPosition)
+      searchPattern(maxPosition+1, ed, dStrich, patternLength, sa, g, is, size_is, ie);
+  }
+ }
+  
+ void getFactors(int startIndex, int patternLength, int g[], int is[], int size_is, int ie[]){	//ToDo: O(1+occ)????
+		for(int i = 0; i<size_is; i++){
+			if(is[i] <= startIndex){
+				if(ie[i] >= startIndex+patternLength-1){
+					cout << "Faktor: " << g[i] << endl;
+				}
+			}
+			else{
+				break;
+			}
+		}
+}
+
+//ToDo: Faktor -> Startposition im String
+
+*/
