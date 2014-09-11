c25bc7458102633418d39a2c3f8b1a5b187b83b8
sopasle <oliver.pasler@uni-ulm.de>
2014-09-11 16:34:06 +0200

    rohe find methode in main hinzugefügt

--- test_FRLZSI.cpp
+++ test_FRLZSI.cpp
@@ -1,11 +1,11 @@
 #include "FRLZSI.hpp"
-
+#include <string>
 
 int main(){
 
 	/*Beispiel aus dem Paper*/
 	string r = "ACGTGATAG";
-	string pattern = "TT";
+	string pattern = "GA";
 	vector<string> s; // hat alle S-Strings
 	s.push_back("TGATAGACG");
 	s.push_back("GAGTACTA");
@@ -15,5 +15,18 @@ int main(){
 	FRLZSI t1(r,s);
 	t1.search_pattern(pattern);
 		
+		/* find als TEstmethode */
+					//012345678901234567890123456
+	std::string str ("TGATAGACGGAGTACTAGTACGTAGGA");
+	
+	int occurrences = 0;
+	string::size_type start = 0;
+
+	while ((start = str.find(pattern, start)) != string::npos) {
+		cout << start << endl;
+    ++occurrences;
+    start += pattern.length(); // see the note
+    
+}
 	return EXIT_SUCCESS;
 }
